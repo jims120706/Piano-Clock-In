db7b5e410ea3220b69725a709f9aafaac90f45ec
liangjiajun <liangjiajun@yaeher.com>
2020-10-17 10:53:37 +0800

    feat：修改请求方法的loading配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,3 @@
-
 import {
   curry
 } from './utils';
@@ -28,11 +27,11 @@ function _request(method = 'GET', options = {}) {
   });
   let url = options.url || '';
   let data = options.data || {};
-  let useLoading = '' + options.useLoading ? options.useLoading : true;
+  let useLoading = ('' + options.useLoading === 'false' || '' + options.useLoading === 'true') ? options.useLoading : true;
   const {
     responseType = 'text',
-    isToken = true, // 是否需要token请求
-    loadingTitle = '加载中'
+      isToken = true, // 是否需要token请求
+      loadingTitle = '加载中'
   } = options;
   /**
    * 如果参数必须要放在url中，对url进行处理
@@ -60,7 +59,6 @@ function _request(method = 'GET', options = {}) {
     if (useLoading) {
       uni.showLoading({
         title: loadingTitle,
-        icon: 'none'
       })
     }
     uni.request({
